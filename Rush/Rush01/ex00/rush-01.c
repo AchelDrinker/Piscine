f722aaaffd0dcc61a30dbb9d7c6c29b59f52f356
AchelDrinker <99177027+AchelDrinker@users.noreply.github.com>
2022-02-23 11:30:17 +0100

    Add files via upload

---
+++ Rush/Rush01/ex00/rush-01.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush-01.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: humartin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/22 12:35:24 by humartin          #+#    #+#             */
+/*   Updated: 2022/01/22 14:39:36 by humartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+ft_putchar(char a)
+{
+	write(1, &a, 1);
+}
+
+ft_rush01(
+
+		tab[4, 4]
+
+		tab [1, 4] = argv[1] + argv[5]
+	   	tab [2, 4] = argv[2] + argv[6]
+   		tab [3, 4] = argv[3] + argv[7]
+		tab [4, 4] = argv[4] + argv[8]
+
+		tab [1, 3] = argv[1] + argv[13]
+	   	tab [2, 3] = argv[2] + argv[14]
+   		tab [3, 3] = argv[3] + argv[15]
+		tab [4, 3] = argv[4] + argv[16]
+
+		tab [1, 2] = argv[1] + argv[5]
+	   	tab [2, 2] = argv[2] + argv[6]
+   		tab [3, 2] = argv[3] + argv[7]
+		tab [4, 2] = argv[4] + argv[8]
+
+		tab [1, 1] = argv[1] + argv[9]
+	   	tab [2, 1] = argv[2] + argv[10]
+   		tab [3, 1] = argv[3] + argv[11]
+		tab [4, 1] = argv[4] + argv[12]
+
+
+
+
+
+
+
+
+
+
+int main(int argc, **char argv)
+{
+	char *str = argv[1];
+	char *str1 = argv[2];
+	char *str2 = argv[1];
+	char *str3 = argv[1];
+	char *str4 = argv[1];
+	char *str5 = argv[1];
+	char *str6 = argv[1];
+	char *str7 = argv[1];
+	char *str8 = argv[1];
+	char *str9 = argv[1];
+	char *str10 = argv[1];
+	char *str11 = argv[1];
+	char *str12 = argv[1];
+	char *str13 = argv[1];
+	char *str14 = argv[1];
+	char *str15 = argv[1];
+
+	ft_rush01(
+}
